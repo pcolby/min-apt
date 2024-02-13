c22d65fd0c990879178a521328067f3398b56b3b
Paul Colby <git@colby.id.au>
2024-02-13 21:08:05 +1100

    Clean-up a little

--- minimise.sh
+++ minimise.sh
@@ -7,29 +7,25 @@ declare -A seen
 
 function getDeps {
   echo "Getting deps for $1" >&2
-  dependencies=$(
-    apt-cache depends "$1" | gawk -- '/^ *[|]?Depends:/ {if (substr(prev,0,1)!="|")print $2;prev=$1}'
-  )
-  for dependency in ${dependencies}; do
-    [[ ! "${dependency}" =~ ^\<([^:]+)(:[^:]+)?\>$ ]] || dependency=${BASH_REMATCH[1]}
-    if [[ -v seen[${dependency}] ]]; then (( seen[${dependency}]++ ))
+  local -a deps
+  deps=$(apt-cache depends "$1" | gawk -- '/^ *[|]?Depends:/ {if (substr(prev,0,1)!="|")print $2;prev=$1}')
+  for dep in ${deps}; do
+    [[ ! "${dep}" =~ ^\<([^:]+)(:[^:]+)?\>$ ]] || dep=${BASH_REMATCH[1]}
+    if [[ -v seen[${dep}] ]]; then (( seen[${dep}]++ ))
     else
-      echo "Found dep: ${dependency}" >&2
-      seen[${dependency}]=1
-      getDeps "${dependency}"
+      echo "Found dep: ${dep}" >&2
+      seen[${dep}]=1
+      getDeps "${dep}"
     fi
   done
 }
 
-for packageName in "$@"; do
-  getDeps "$packageName"
-done
-
-for dep in "${!seen[@]}"; do
-  echo "$dep: ${seen[$dep]}" >&2
-done
-
-echo 'Minimal set:' >&2
 for pkg in "$@"; do
-  [[ -v seen[$pkg] ]] || echo "$pkg"
+  getDeps "${pkg}"
 done
+
+echo -n 'apt install' >&2
+for pkg in "$@"; do
+  [[ -v seen["${pkg}"] ]] || echo "${pkg}"
+done | sort | tr '\n' ' '
+echo
