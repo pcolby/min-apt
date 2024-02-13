bc575e1a47c1e90dd13bd1d9f2f90abd7dd5e4bc
Paul Colby <git@colby.id.au>
2024-02-13 20:54:12 +1100

    First working version

--- minimise.sh
+++ minimise.sh
@@ -3,19 +3,33 @@
 set -o errexit -o noclobber -o nounset -o pipefail
 shopt -s inherit_errexit
 
-readarray -td '' packageNames < <(printf '%s\0' "$@" | sort -uz || :)
-for packageName in "${packageNames[@]}"; do
-  echo "${packageName}" >&2
+declare -A seen
+
+function getDeps {
+  echo "Getting deps for $1" >&2
   dependencies=$(
-    apt-cache depends "${packageName}" | gawk -- '/^ *[|]?Depends:/ {if (substr(prev,0,1)!="|")print $2;prev=$1}'
+    apt-cache depends "$1" | gawk -- '/^ *[|]?Depends:/ {if (substr(prev,0,1)!="|")print $2;prev=$1}'
   )
   for dependency in ${dependencies}; do
-    echo "${dependency}"
-    [[ ! "${dependency}" =~ ^\<([^:]+):[^:]+\>$ ]] || dependency=${BASH_REMATCH[1]}
-    # Check if dep has already been seen.
-    # remove this from top list
-    # check children.
+    [[ ! "${dependency}" =~ ^\<([^:]+)(:[^:]+)?\>$ ]] || dependency=${BASH_REMATCH[1]}
+    if [[ -v seen[${dependency}] ]]; then (( seen[${dependency}]++ ))
+    else
+      echo "Found dep: ${dependency}" >&2
+      seen[${dependency}]=1
+      getDeps "${dependency}"
+    fi
   done
+}
 
-#<python3:any>
+for packageName in "$@"; do
+  getDeps "$packageName"
+done
+
+for dep in "${!seen[@]}"; do
+  echo "$dep: ${seen[$dep]}" >&2
+done
+
+echo 'Minimal set:' >&2
+for pkg in "$@"; do
+  [[ -v seen[$pkg] ]] || echo "$pkg"
 done
